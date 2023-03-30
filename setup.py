d438a50da10415c6d6885acb16f96d663efd9232
avidito <av.leonardif@gmail.com>
2023-03-30 08:39:59 +0700

    cli - fix missing blueprint

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+import os
 from setuptools import setup
+
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join("..", path, filename))
+    return paths
+
+data_paths = package_files("airflow_manager/blueprint")
 
 setup(
     name = "airflow-manager",
     version = "0.1",
     packages = ["airflow_manager", "airflow_manager.modules"],
     package_dir = {"airflow_manager": "airflow_manager"},
-    package_data = {"airflow_manager": ["blueprint/*"]},
+    package_data = {"airflow_manager": data_paths},
     include_package_data = True,
     install_requires = [
         "Click",
